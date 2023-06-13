19b6a433184aa7993215f8e10513203b9a2a2628
ber <93647413+bertapalahi@users.noreply.github.com>
2023-06-13 17:24:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+<script>
+  if ("loading" in HTMLImageElement.prototype) {
+    console.log("El navegador soporta `lazy-loading`...")} else {
+    console.log("`lazy-loading` no soportado...")}
+</script>
