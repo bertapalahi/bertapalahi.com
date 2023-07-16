602b37a3412b8040647e7f0fa62a2fb9d5d10ee0
ber <93647413+bertapalahi@users.noreply.github.com>
2023-07-16 13:48:02 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-
-<script>
-  if ("loading" in HTMLImageElement.prototype) {
-    console.log("El navegador soporta `lazy-loading`...")} else {
-    console.log("`lazy-loading` no soportado...")}
-</script>
